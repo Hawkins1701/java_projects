ea6b74de69783543c5a5c636e20df8680aab5ebe
Michael <mknudsen84@gmail.com>
2014-01-10 00:31:28 -0800

    First

---
+++ Coin_Counting_and_Commission_Calculator/src/CCount_Calc/CCount_Calc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CCount_Calc;
+
+/**
+ *
+ * @author Mikey
+ */
+public class CCount_Calc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CCount_Calc_Form myForm = new CCount_Calc_Form();
+        myForm.setVisible(true);
+    }
+}
