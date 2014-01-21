4cb5221d2e3ae0484e3f1f526fe5862c74c3efe8
Michael <mknudsen84@gmail.com>
2014-01-20 19:29:48 -0800

    Initial commit

---
+++ Calculating_Volume/src/calc_volume/Volume_Calc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc_volume;
+
+/**
+ *
+ * @author michael.knudsen
+ */
+public class Volume_Calc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // instantiate and display the form 
+        shapeBuilderGUI myForm = new shapeBuilderGUI(); 
+        myForm.setVisible(true);
+    }
+}
